326933d35aea27f5ecbb3d6f2a7a35768d0a26f8
asawo <asawo@pc1108.local>
2019-04-01 22:05:41 +0900

    added things

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
   //set and store variables at the start or else tweet button can't pull quotes
   var varQuote;
   var varAuthor;
